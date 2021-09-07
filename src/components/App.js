ef928114226e114dbfd0d029fd3e15b7b182aaa1
nilanjan83 <nilanjandey65@gmail.com>
2021-09-07 23:00:54 +0530

    nil

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,23 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    render() {
+    constructor(props){  
+        super(props); 
+        this.state = { attribute : new Date().toLocaleTimeString() }; 
+   }
+   componentDidMount(){
+      this.timee = setInterval(()=> {this.setState({attribute: new Date().toLocaleTimeString()})},1000); 
+   }
+   componentWillUnmount(){
+       clearInterval(this.timee);
+   }
 
+    render() {
         return(
             <>
-               
+            <div className="Clock">
+                <h3 id="time">{this.state.attribute}</h3>
+            </div>
             </>
         )
     }
